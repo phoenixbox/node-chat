cd3f38f094d90a55728f6beb7778aa13fe01cfcb
Shane Rogers <rogerssh@tcd.ie>
2015-01-07 13:43:35 +0000

    simple UI setup

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ var server = http.createServer(function(request, response){
 	serveStatic(response, cache, absPath);
 });
 
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
 server.listen(3000, function() {
 	console.log('Server listening on port 3000');
 });
